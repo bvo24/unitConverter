31c41cafd479afcbf0d90f88fa3d41d7fc288f37
bvo24 <brianvo2020@gmail.com>
2024-04-24 16:19:20 -0400

    Initial Commit

---
+++ unitConverter/unitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  unitConverterApp.swift
+//  unitConverter
+//
+//  Created by Brian Vo on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct unitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
